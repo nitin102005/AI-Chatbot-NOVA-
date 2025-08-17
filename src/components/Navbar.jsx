189fb09745c4ac52632173c66d158f0229240e4d
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2025-08-17 09:15:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar({ onGoHome }) {
         <div className='cursor-pointer '>ABOUT</div>
         <div className='cursor-pointer'>SERVICES</div>
         <div className='cursor-pointer'>DOCS</div>
-        <div className='w-fit ml-94 bottom-4 relative bg-black/20 text-white rounded-xl border border-white/10 shadow-lg p-2 px-4 gap-1.5 flex items-center justify-center'>
+        <div className='w-fit hidden lg:flex ml-94 bottom-4 relative bg-black/20 text-white rounded-xl border border-white/10 shadow-lg p-2 px-4 gap-1.5 items-center justify-center'>
         <FaReact size={30} className="text-blue-400" />
   <h1 className='text-[13px]'>Built with <br /> React</h1>
   </div>
